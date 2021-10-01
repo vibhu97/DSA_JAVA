36cb1c634cd34bde6cbe54467cf1076077092007
Administrator <Administrator@172.31.11.16>
2021-10-01 17:12:44 +0000

    getLast,getFirst,getAt added

--- Data-Structures/src/linkedlist/Main.java
+++ Data-Structures/src/linkedlist/Main.java
@@ -1,13 +1,23 @@
 package linkedlist;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SinglyLinkedList sl=new SinglyLinkedList();
+		//System.out.println(sl.getFirst());
+		
+		System.out.println(sl.getAt(1));
+		sl.add(0);
 		sl.append(10);
 		sl.append(20);
+		System.out.println(sl.getAt(1));
 		sl.append(30);
 		sl.append(40);
 		sl.append(50);
+		sl.add(60);
+		//System.out.println(sl.getAt(1));
+		//System.out.println(sl.getFirst());
+		//System.out.println(sl.getLast());
+		System.out.println(sl.getAt(0));
 		sl.traverse();
 	}
 
--- Data-Structures/src/linkedlist/SinglyLinkedList.java
+++ Data-Structures/src/linkedlist/SinglyLinkedList.java
@@ -31,5 +31,51 @@ public class SinglyLinkedList {
 			temp=temp.next;
 		}
 	}
+	
+	public  void add(int item) {
+		Node n=new Node();
+		n.data=item;
+		if(size==0) {
+			head=n;
+			tail=n;
+			size++;
+		}
+		else {
+			n.next=head;
+			head=n;
+			size++;
+		}
+	}
+	public int getFirst() throws Exception {
+		if(size==0) {
+			throw new Exception("LL is Empty");
+		}
+		else {
+			return head.data;
+		}
+	}
+	
+	public int getLast() throws Exception {
+		if(size==0)
+			throw new Exception("LL is Empty");
+		else
+			return tail.data;
+			
+	}
+	public int getAt(int index) throws Exception {
+		if(size==0)
+			throw new Exception("LL is Empty");
+		else if(index<0 || index>=size)
+			throw new Exception("Invalid Index");
+		else {
+			Node temp=new Node();
+			temp=head;
+			for(int i=0;i<index;i++)
+				temp=temp.next;
+			return temp.data;
+				
+		}
+			
+	}
 
 }
