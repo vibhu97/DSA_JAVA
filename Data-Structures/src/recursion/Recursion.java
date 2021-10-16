a0cc752e382807e14f3da34c8621ae2606537747
Administrator <Administrator@172.31.11.16>
2021-10-16 08:10:48 +0000

    Fact,Fib,Pow,isSorted added

--- Data-Structures/src/recursion/Recursion.java
+++ Data-Structures/src/recursion/Recursion.java
@@ -5,7 +5,12 @@ public class Recursion {
 		//printDecreasing(5);
 		//printIncreasing(5);
 		//printMixed(5);
-		printSkip(5);
+		//printSkip(5);
+		//System.out.println(fact(6));
+		//System.out.println(pow(5,3));
+		//System.out.println(fib(6));
+		//int a[]= {1,2,3,4,5,4};
+		//System.out.println(isSorted(a, 0));
 	}
 	
 	// 5 4 3 2 1
@@ -41,6 +46,32 @@ public class Recursion {
 		if(n%2==0)
 			System.out.println(n);
 		
+	}
+	
+	
+	public static int fact(int n) {
+		if(n==1)
+			return 1;
+		return n*fact(n-1);
+	}
+	public static int pow(int x,int n) {
+		if(n==0)
+			return 1;
+		return x*pow(x,n-1);
+	}
+	public static int fib(int n) {
+		if(n==0 || n==1)
+			return n;
+		return fib(n-1)+fib(n-2);
+	}
+	
+	public static boolean isSorted(int a[],int i) {
+		if(i==a.length-1)
+			return true;
+		if(a[i]>a[i+1])
+			return false;
+		else
+			return isSorted(a, i+1);
 	}
 
 }
