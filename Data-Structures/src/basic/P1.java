bc93e5abc82619cc0ccfc9739f5f1b2555845ed7
vaibh <vaibh@DESKTOP-84AH4FU>
2021-12-19 22:23:20 -0800

    Basic

---
+++ Data-Structures/src/basic/P1.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.*;
+
+public class P1 {
+
+  public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+  }
+}
